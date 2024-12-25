4bded62e788374220e4ff7b001402e4a66b55a45
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2024-12-26 02:27:25 +0700

    Fix escape string for sqlite

--- SQLite.php
+++ SQLite.php
@@ -4,6 +4,14 @@ namespace koolreport\querybuilder;
 
 class SQLite extends SQL
 {
-    protected $escapeValue = false;
+    protected $escapeValue = true;
     protected $identifierQuotes=array("`","`");//For table name and column name
+
+    protected function escapeString($string)
+    {
+        if($this->escapeValue) {
+            return str_replace("'","''",$string);
+        }
+        return $string;
+    }
 }
